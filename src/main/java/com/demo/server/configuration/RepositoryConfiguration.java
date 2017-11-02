d5efe15f347a031575584397db9113d760a8e57c
Evgeniy <zhenyakurilo@gmail.com>
2017-11-02 02:36:35 +0200

    show ids

---
+++ src/main/java/com/demo/server/configuration/RepositoryConfiguration.java
@@ -0,0 +1,14 @@
+package com.demo.server.configuration;
+
+import com.demo.server.domain.Contact;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+@Configuration
+public class RepositoryConfiguration extends RepositoryRestConfigurerAdapter {
+  @Override
+  public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+    config.exposeIdsFor(Contact.class);
+  }
+}
